250af2d34e0d455fd489eccc57a680d93cee93d1
Marijn <marijnstam@live.nl>
2021-07-30 17:15:36 +0200

    Succesfully retrieve albums from an AOTY page and add to playlist

--- Source/retrieveAlbums.py
+++ Source/retrieveAlbums.py
@@ -2,18 +2,18 @@ import sys
 from bs4 import BeautifulSoup
 import urllib.request
 
+LINK_URI_INDEX = 30
 
-class Albums:
-
-    def getAlbum(self):
+def getAlbum():
         
-        req = urllib.request.Request(url='https://www.albumoftheyear.org/ratings/6-highest-rated/2021/1', headers={'User-Agent': 'Mozilla/5.0'})
-        html = urllib.request.urlopen(req).read()
-    
-        soup = BeautifulSoup(html, features="html.parser")
+    req = urllib.request.Request(url='https://www.albumoftheyear.org/ratings/6-highest-rated/2021/1', headers={'User-Agent': 'Mozilla/5.0'})
+    html = urllib.request.urlopen(req).read()
 
-        table = soup.findAll('a',attrs={"data-track-action":"Spotify"})
-        albumList = []
-        for idx, x in enumerate(table):
-            albumList.append(x.get('href'))
-        return albumList
+    soup = BeautifulSoup(html, features="html.parser")
+
+    table = soup.findAll('a',attrs={"data-track-action":"Spotify"})
+    albumList = []
+    for idx, x in enumerate(table):
+        uriLink = x.get('href')
+        albumList.append(uriLink[LINK_URI_INDEX:])
+    return albumList
--- Source/updatePlaylist.py
+++ Source/updatePlaylist.py
@@ -1,16 +1,28 @@
 import sys
+import json
+from typing import List
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from retrieveAlbums import Albums
+from retrieveAlbums import getAlbum
+
+PLAYLIST_ID = '5hwjLoGifPEGBrBmNZxa0X'
 
 def main():
 
     spotifyInterface = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="playlist-modify-public,user-library-read"))
  
-    albumGetter = Albums()
-    albumList = albumGetter.getAlbum()
 
-    print(albumList)
+    albumList = getAlbum()
+    
+    for x in albumList:
+        spotifyAlbumList = spotifyInterface.album(x)
+
+    for x in albumList:
+        album = spotifyInterface.album_tracks(x)
+        for y in album['items']:
+            spotifyInterface.playlist_add_items(PLAYLIST_ID, [y['id']])
+        
+
 
 
 if __name__ == '__main__':
