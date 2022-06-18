c7c50b0d775b1dd9a1fe8e6d54c4a08d6d15b3b9
Marijn Stam <marijnstam@betonic.nl>
2022-06-18 14:29:03 +0200

    web: rename the modules and change webscraper to return list of album names instead of uris
    the new webinterface of aoty requires you to do some javascript stuff to retrieve the album uris, as they are in dropdown menus. will get uris through spotipy now

---
+++ Source/WebScaper.py
@@ -0,0 +1,21 @@
+import sys
+from bs4 import BeautifulSoup
+import urllib.request
+
+LINK_URI_INDEX = 30
+
+def getAlbum():
+        
+    req = urllib.request.Request(url='https://www.albumoftheyear.org/ratings/6-highest-rated/2022/1', headers={'User-Agent': 'Mozilla/5.0'})
+    html = urllib.request.urlopen(req).read()
+    soup = BeautifulSoup(html, features="html.parser")
+
+    #Retrieve the names of all Artist - Album entries on the webpage
+    #Append them into a list
+    albumList = []
+    for name in soup.find_all("meta", itemprop="name"):
+        albumList.append(name.get("content", None))
+
+    return albumList
+
+getAlbum()
