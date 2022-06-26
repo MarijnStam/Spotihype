7bd3051b34e80d68ff631cf6f56854f84ce716b4
Marijn Stam <marijnstam@betonic.nl>
2022-06-26 22:12:10 +0200

    db: make functional uri checking in db before adding album

--- Source/DiscordBot.py
+++ Source/DiscordBot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import find_dotenv, load_dotenv
+import sqlite3
 
 import WebScaper as wb
 import Spotify as sp
@@ -43,24 +44,29 @@ async def addAlbums(ctx, arg: int = 5):
     albumList = wb.getAlbums()
 
     playlistName, playlistLink = sp.getPlaylist(sp.AOTY_PLAYLIST_ID)
+    
+    index = 0
+    addedAlbumCount = 0
 
-    for i in range(0, amount):
+    while (addedAlbumCount < amount):
         #Retrieve the spotify album from the artist/album info from the webscraper. add the entry to the local db 
         try:
-            album = sp.Album(albumList[i])
-            sp.addAlbumToPlaylist(album.uri, sp.AOTY_PLAYLIST_ID)
+            album = sp.Album(albumList[index])
         except sp.NotFoundError as e:
             print(e)
             continue
 
         #Try adding album details to db
-        # try:
-        #     db.addAlbum(album.artist, album.name, album.uri)
-        #     
-        # #If we encounter this error, it means the ID is already in the db, skip this album and increment amount to add
-        # except sqlite3.Error as e:
-        #     amount = amount + 1
-        #     continue
+        try:            
+            db.addAlbum(album.artist, album.name, album.uri)
+            sp.addAlbumToPlaylist(album.uri, sp.AOTY_PLAYLIST_ID)
+            addedAlbumCount = addedAlbumCount + 1
+        #If we encounter this error, it means the ID is already in the db, skip this album and increment amount to add
+        except sqlite3.Error as e: 
+            continue
+
+        finally:
+            index = index + 1
 
 #MAKE INTO ONE BIG EMBED INSTEAD, TOO MUCH GOING ON
         embed=discord.Embed(
