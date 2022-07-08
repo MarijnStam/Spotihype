18ac3eb056646471b55ead266e6de0f7939052a2
Marijn Stam <marijnstam@betronic.nl>
2022-07-08 13:15:05 +0200

    test: still allow for spotipy tests to be run locally

--- test/test_spotify.py
+++ test/test_spotify.py
@@ -1,11 +1,15 @@
 import pytest
+import os
 
 from Source import Spotify as sp
+
+path = os.path.dirname(os.path.abspath(__file__))
+skip = (path == "/home/runner/work/Spotihype/Spotihype/test")
 
 name = [("Black Country - New Road", "Ants From Up There"), ()]
 uriName = '41ycYGcnhkDb3pFkL8vSPJ'
 
-@pytest.mark.skip(reason="Cannot test OAuth2 workflow on GitHub Actions")
+@pytest.mark.skipif(skip, reason="Spotipy tests are not supported on CI")
 @pytest.mark.parametrize("name, uriName", [
     (name, uriName),
     (name, None),
@@ -18,7 +22,7 @@ def test_album_init(name, uriName):
     assert (album.name) != None
     assert (album.uri) != None
 
-@pytest.mark.skip(reason="Cannot test OAuth2 workflow on GitHub Actions")
+@pytest.mark.skipif(skip, reason="Spotipy tests are not supported on CI")
 def test_get_playlist():
     name, link = sp.getPlaylist(sp.AOTY_PLAYLIST_ID)
     assert name != None
